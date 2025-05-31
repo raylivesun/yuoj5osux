ac267af4fa871f5c2a69b0d969b6a61f82de410a
raylivesun <162396999+raylivesun@users.noreply.github.com>
2025-05-31 05:25:32 -0300

    the main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <c++/11/algorithm>
+#include <c++/11/array>
+#include <c++/11/cfloat>
+#include <c++/11/cassert>
+#include <c++/11/chrono>
+#include <c++/11/cstdarg>
+#include <c++/11/cstdio>
+#include <c++/11/cstdlib>
+#include <c++/11/numbers>
+#include <c++/11/iostream>
+#include <c++/11/future>
+#include <c++/11/debug/assertions.h>
+#include <c++/11/debug/debug.h>
+
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
